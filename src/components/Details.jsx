9cedac2cdfa50a15daf850fd7f4e45ce5b5ec44f
Ayuapry <ayuapriyanti0304@gmail.com>
2022-09-29 13:08:19 +0700

    edited details page

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState  } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import {Navbar} from './Navbar'
+import {Footer} from './Footer'
 
 export const Details = () => {
     const [foods, setFoods] = useState([]);
@@ -22,52 +23,36 @@ export const Details = () => {
 
   return (
     <div>
-    
-              <Navbar />
-              
-    <div  className='w-full bg-white py-16 px-4'>
+        <Navbar />          
+    <div  className='w-full bg-white py-16 px-4 '>
+        {foods.map((item, index) => (
+        <div key={index}>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 mt-5'>
+            <img className='w-[500px] mx-auto my-4' src={item.strMealThumb} alt='/' />
+        <div className='flex flex-col justify-center mt-5'>
+            <p className='text-[#00df9a] font-bold '>{item.strArea}</p>
+            <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>{item.strMeal}</h1>
+            <span className='font-bold'>Ingrediengts : </span>
+                <li>{item.strIngredient1}</li>
+                <li>{item.strIngredient2}</li>
+                <li>{item.strIngredient3}</li>
+                <li>{item.strIngredient4}</li>
+                <li>{item.strIngredient5}</li>
+                <li>{item.strIngredient6}</li>
+                <li>{item.strIngredient7}</li>
+            <p><br />
+                <span className='font-bold'>Instruction :</span><br />
+                {item.strInstructions}
+            </p>
+            <Link to="/" className='bg-black text-white w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-center'>Back To Menu</Link>
+        </div>
+    </div>
+    </div>
+    ))}
+    </div>
+    <Footer />
+    </div>
 
-            {foods.map((item, index) => (
-                <div key={index}>
-        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-                {/* <img src={item.strMealThumb} alt="/"  className='w-[500px] mx-auto my-4'/>
-                <div className='flex flex-col justify-center'>
-                <p className='text-[#00df9a] font-bold '>{item.strArea}</p>
-                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>{item.strMeal}</h1>
-                        {/* <li>Area: </li>
-                        <li>Instruction: 
-                            <p>{item.strInstructions}</p>
-                        </li>
-                        <ul>
-                            <li>Ingredients</li>
-                            <li>{item.strIngredient1}</li>
-                            <li>{item.strIngredient2}</li>
-                            <li>{item.strIngredient3}</li>
-                            <li>{item.strIngredient4}</li>
-                            <li>{item.strIngredient5}</li>
-                            <li>{item.strIngredient6}</li>
-                            <li>{item.strIngredient7}</li>
-                        </ul> */}
-                        {/* </div> */}
-       
-  <img className='w-[500px] mx-auto my-4' src={item.strMealThumb} alt='/' />
-  <div className='flex flex-col justify-center'>
-    <p className='text-[#00df9a] font-bold '>{item.strArea}</p>
-    <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-      molestiae delectus culpa hic assumenda, voluptate reprehenderit
-      dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
-      eveniet ex deserunt fuga?
-    </p>
-    <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>
-  </div>
-</div>
-</div>
-  ))}
-                </div>
-    //     </div>
-    // </div>
 
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import {BsFillCartFill} from 'react-icons/bs'
 
 export const Navbar = () => {
@@ -7,9 +8,9 @@ export const Navbar = () => {
 
       {/* Left side */}
       <div className='flex items-center'>
-        <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
+        <Link to="/" className='text-2xl sm:text-3xl lg:text-4xl px-2'>
           Best <span className='font-bold'>Eats</span>
-        </h1>
+        </Link>
         <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
           <p className='bg-black text-white rounded-full p-2'>Delivery</p>
           <p className='p-2'>Pickup</p>
